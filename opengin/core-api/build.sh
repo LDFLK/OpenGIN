444a150e92962b163b4a8659d555e0784430e5df
vibhatha <vibhatha@users.noreply.github.com>
2026-01-29 07:37:16 +0000

    deploy: 37602e333498fdf89fe270d0785eb1f2db6bd7eb

--- opengin/core-api/build.sh
+++
@@ -1,8 +0,0 @@
-# Copyright 2025 Lanka Data Foundation
-# SPDX-License-Identifier: Apache-2.0
-
-echo "Building all packages..."
-go build -v ./...
-echo "Building core-service binary..."
-go build -v -o core-service cmd/server/service.go cmd/server/utils.go
-echo "Build complete!"
--- opengin/tests/e2e/README.md
+++
@@ -1,9 +0,0 @@
-## Setup 
-
-```bash
-cd design/ingestion-api && source .env && bal run
-cd ../../
-cd opengin/read-api && source .env && bal run
-python basic_core_tests.py
-python basic_read_tests.py
-```
